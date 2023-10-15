643ccf564a290fe771fee565323e701108d975d7
Shi Er Lee <eve@Shis-MacBook-Pro.local>
2023-10-15 18:29:08 +0800

    phonebook contact form

--- phonebook/src/App.tsx
+++ phonebook/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 // import axios from "axios";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function App() {
 
